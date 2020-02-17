05a355dc579d801068cad3c1b762b5dcd2e3f15a
Frank Iriarte <jurrish@For-the-screen-is-dark-and-full-of-errors.local>
2020-02-17 13:33:01 -0800

    more bitmap conversion

--- 3_lecture/demos/fs/scratch.js
+++ 3_lecture/demos/fs/scratch.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 
-fs.readFile('./png-bitmap-4.bmp', (err, data) => {
+fs.readFile('./hammherhead.bmp', (err, data) => {
 
 
   //turn black
   //find the bit that determines color
-  // for(let i = 8; i < data.length; i++) {
-  //   data[i] = 0;
-  // }
+  for(let i = 16; i < data.length; i++) {
+    data[i] = 0;
+  }
 
-  console.log(data.readInt8(0));
+  // console.log(data.readInt8(0));
 
-  fs.writeFile('./png-bitmap-5.bmp', data, (err, data) => {
+  fs.writeFile('./hammherhead-2.bmp', data, (err, data) => {
     console.log('yo');
   })
 })
