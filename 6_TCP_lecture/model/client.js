9b08698441732167333bb1dda42c02c7a33fb9c1
Frank Iriarte <jurrish@For-the-screen-is-dark-and-full-of-errors.local>
2020-02-23 16:42:13 -0800

    past commits

--- 6_TCP_lecture/model/client.js
+++ 6_TCP_lecture/model/client.js
@@ -1,16 +1,16 @@
 'use strict';
 
 //unique id for each user
-const uuid = require('uuid/v4');
+const uuidv4 = require('uuid/v4');
 
 class User {
   //pass in the socket to keep track of what's open to read/write
   constructor(socket) {
-    let id = new uuid();
+    let id = new uuidv4();
     this.id = id;
     this.nickname = `user_${id}`;
     this.socket = socket;
   }
 }
 
-module.exprts = User;
+module.exports = User;
--- 6_TCP_lecture/server.js
+++ 6_TCP_lecture/server.js
@@ -16,6 +16,54 @@ const PORT = process.env.PORT || 3000;
 //make a new server using net we required in
 const server = net.createServer();
 
+//new instance of EE
+const ee = new EE();
+
+//create a pool for connections
+const pool = [];
+
+//setup a connection event - anytime a connection occurs
+server.on('connection', (socket) => {
+  const client = new Client(socket);
+  pool.push(client);
+  client.socket.write('Welcome to the chat room!\n');
+
+  //listen for data coming in
+  socket.on('data', (data) => {
+    //data is a buffer
+    // console.log('this is the data',data);
+    // console.log('this is the data.toString', data.toString());
+
+  //first command: @all - send a message to everyone in the chat room
+  //data.toString is what we type
+  //split at / and
+  const command = data.toString().split(' ').shift().trim(); //removes the first part @all
+
+  //on @ symbol
+  if(command.startsWith('@')) {
+    const restOfCommand = data.toString().split(' ').splice(1).join(' ');
+    ee.emit(command, client, restOfCommand);
+    return;
+  }
+  //default behavior
+
+
+  })
+
+})
+
+//register event listeners that we've created above (@all)
+ee.on('@all', (client, message) => {
+  pool.forEach(user => {
+    user.socket.write(`${client.nickname} : ${message}`);
+  })
+});
+
+ee.on('@nickname', (client, nickname) => {
+  client.nickname = nickname;
+  client.socket.write(`your nickname has been changed to ${nickname}`);
+})
+
 
 
 //make sure server is listening!
